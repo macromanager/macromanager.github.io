b6e1c4e216e392ea2ab53b38e816c4f5c725be3a
evan <evandsilverstein@gmail.com>
2020-01-29 00:06:19 -0500

    added new installer

--- src/services/ProductInfoService.ts
+++ src/services/ProductInfoService.ts
@@ -64,11 +64,19 @@ export class ProductInfoService {
     }
 
     private initializeInstallers(): Installer[]{
+      
       var downloadsPath = this.config.getUploadsPath() + '/macro-manager-installers';
       var title = 'MacroManager';
-      var version = '1.1.68.0';
-      var url = downloadsPath + '/MacroManager_v1.1.68.0.msi';
+      var version = '1.1.69.0';
+      var url = downloadsPath + '/MacroManager_v1.1.69.0.msi';
       var installer1 = new Installer(title + "_v" + version, version, url);
+
+      
+      // var downloadsPath = this.config.getUploadsPath() + '/macro-manager-installers';
+      // var title = 'MacroManager';
+      // var version = '1.1.68.0';
+      // var url = downloadsPath + '/MacroManager_v1.1.68.0.msi';
+      // var installer1 = new Installer(title + "_v" + version, version, url);
 
       // var title = 'MacroManager';
       // var version = '1.1.64.0';
