3926288e8e70774e3aabd94f8f19f3a064830802
evan <evandsilverstein@gmail.com>
2020-05-12 11:17:29 -0400

    bundled latest changes

---
+++ dist/bundle/polyfills.js
@@ -0,0 +1,18 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[3],{
+
+/***/ 277:
+/***/ (function(module, exports, __webpack_require__) {
+
+// import 'core-js/es6';
+// import 'core-js/es7/reflect';
+__webpack_require__(196);
+
+__webpack_require__(233);
+
+if (true) {// Production
+} else {}
+
+/***/ })
+
+},[[277,0,1]]]);
+//# sourceMappingURL=polyfills.js.map
